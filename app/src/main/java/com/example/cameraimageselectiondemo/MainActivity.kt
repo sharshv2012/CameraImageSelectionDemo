94d3a2ae009c037b69279a249021e471a768ef94
Harsh <sharshvardhan759@gmail.com>
2023-09-11 17:11:43 +0530

    commit : as startActivityResultLauncher is deprecated so changed the whole code which is crisp and clean their is a minor issue , in the start i need to click the button twice to trigger the action.

--- app/src/main/java/com/example/cameraimageselectiondemo/MainActivity.kt
+++ app/src/main/java/com/example/cameraimageselectiondemo/MainActivity.kt
@@ -2,19 +2,27 @@ package com.example.cameraimageselectiondemo
 
 import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.Settings
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.cameraimageselectiondemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var binding : ActivityMainBinding? = null
+    private lateinit var imageCaptureLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
     companion object{
         private const val CAMERA_PERMISSION_CODE = 1
         private const val CAMERA_REQUEST_CODE = 2
@@ -24,18 +32,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+
+
+
+
+
         binding?.btnCamera?.setOnClickListener {
-            if(ContextCompat.checkSelfPermission(
-                this,
+            binding?.btnCamera?.setOnClickListener {
+                if (ContextCompat.checkSelfPermission(
+                        this@MainActivity,
                         Manifest.permission.CAMERA
                     ) == PackageManager.PERMISSION_GRANTED
-                ){
+                ) {
+                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    imageCaptureLauncher.launch(intent)
+                } else {
+                    permissionLauncher.launch(Manifest.permission.CAMERA)
+                }
+            }
+
+
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
                 val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivityForResult(intent , CAMERA_REQUEST_CODE)
-            }else{
-                ActivityCompat.requestPermissions(this ,
-                    arrayOf(Manifest.permission.CAMERA) ,
-                    CAMERA_PERMISSION_CODE )
+                imageCaptureLauncher.launch(intent)
+            } else {
+                showRationalDialogForPermissions()
+
+            }
+        }
+        imageCaptureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val thumbNail: Bitmap = result.data?.extras?.get("data")as Bitmap
+                binding?.ivImage?.setImageBitmap(thumbNail)
             }
         }
 
@@ -44,34 +75,31 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == CAMERA_PERMISSION_CODE){
-            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)  // captures image
-                startActivityForResult(intent , CAMERA_REQUEST_CODE)
+    private fun showRationalDialogForPermissions(){
+        AlertDialog.Builder(this).setMessage("" +
+                "It looks like you have turned off permissions required "
+                + "for this feature. It can be enabled under the " +
+                "Applications Settings")
+            .setPositiveButton("GO TO SETTINGS")
+            {_ , _ ->  // _ , _ is used because the parameter variables aren't used below.
+                try { // in these cases always use try catch blocks.
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package" ,packageName , null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e : ActivityNotFoundException){
+                    e.printStackTrace()
+                }
 
-            }else{
-                Toast.makeText(
-                    this ,
-                    "Permission Denied" ,
-                    Toast.LENGTH_SHORT
-                ).show()
             }
-        }
+            .setNegativeButton("Cancel"){dialog , which -> // here which can be replaced by an _ as it isn't used.
+                dialog.dismiss()
+            }
+            .setCancelable(false) // will not allow user to cancel by clicking on the remaining area
+            .show()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK){
-            if (requestCode == CAMERA_REQUEST_CODE){
-                val thumbNail : Bitmap = data!!.extras!!.get("data")as Bitmap
-                binding?.ivImage!!.setImageBitmap(thumbNail)
-            }
-        }
-    }
+
+
+
 }
