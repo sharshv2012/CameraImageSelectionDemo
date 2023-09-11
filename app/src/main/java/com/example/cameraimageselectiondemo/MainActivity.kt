a13a5ee4baa9e6fb02f1b3e17be31a4c32fddaac
Harsh <sharshvardhan759@gmail.com>
2023-09-11 21:00:56 +0530

    Commit 1: Solved the double click problem by using onClick Method : )

--- app/src/main/java/com/example/cameraimageselectiondemo/MainActivity.kt
+++ app/src/main/java/com/example/cameraimageselectiondemo/MainActivity.kt
@@ -10,8 +10,11 @@ import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.provider.MediaStore
 import android.provider.Settings
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -19,14 +22,15 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.cameraimageselectiondemo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() , View.OnClickListener {
     private var binding : ActivityMainBinding? = null
     private lateinit var imageCaptureLauncher: ActivityResultLauncher<Intent>
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
-    companion object{
-        private const val CAMERA_PERMISSION_CODE = 1
-        private const val CAMERA_REQUEST_CODE = 2
+
+    constructor(parcel: Parcel) : this() {
+
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -37,22 +41,11 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        binding?.btnCamera?.setOnClickListener {
-            binding?.btnCamera?.setOnClickListener {
-                if (ContextCompat.checkSelfPermission(
-                        this@MainActivity,
-                        Manifest.permission.CAMERA
-                    ) == PackageManager.PERMISSION_GRANTED
-                ) {
-                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                    imageCaptureLauncher.launch(intent)
-                } else {
-                    permissionLauncher.launch(Manifest.permission.CAMERA)
-                }
-            }
+        binding?.btnCamera?.setOnClickListener(this@MainActivity)
 
 
-        }
+
+
 
         permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
             if (isGranted) {
@@ -99,7 +92,26 @@ class MainActivity : AppCompatActivity() {
             .show()
     }
 
+    override fun onClick(v: View?) {
+        when(v!!){
+            binding?.btnCamera ->{
 
+
+                    if (ContextCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.CAMERA
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        imageCaptureLauncher.launch(intent)
+                    } else {
+                        permissionLauncher.launch(Manifest.permission.CAMERA)
+                    }
+
+
+            }
+        }
+    }
 
 
 }
